540dd6fbf1e42d3f4b593c4e012c54b274c2d04c
Kathryn <ktanardy@gmail.com>
2023-05-16 19:54:21 -0700

    initial commit

---
+++ src/main/java/com/example/railwaydemo/RailwaydemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.railwaydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwaydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwaydemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/railwaydemo/RailwaydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.railwaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwaydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
